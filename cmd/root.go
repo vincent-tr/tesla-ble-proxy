2d8ad3c6acfc443b0d4fd52ebf4319faec36e886
vincent-tr <vincent.tr@gmail.com>
2024-07-15 11:53:54 +0200

    clean cli

--- cmd/root.go
+++ cmd/root.go
@@ -2,13 +2,28 @@ package cmd
 
 import (
 	"os"
+	"tesla-ble-proxy/pkg/web"
 
 	"github.com/spf13/cobra"
 )
 
+var address string
+var debug bool
+
 var rootCmd = &cobra.Command{
 	Use:   "tesla-ble-proxy",
 	Short: "tesla-ble-proxy - Tesla BLE Rest API (proxy)",
+	Run:   run,
+}
+
+func init() {
+	rootCmd.Flags().StringVarP(&address, "address", "a", ":80", "Address for the webserver to listen on")
+	rootCmd.Flags().BoolVarP(&debug, "debug", "", false, "Web server debug mode")
+}
+
+func run(_ *cobra.Command, _ []string) {
+
+	web.Serve(address, debug)
 }
 
 func Execute() {
